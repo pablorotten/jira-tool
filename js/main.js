be26a69ff5563d2443442eb914cf28f1fda6738f
PRO <pro@n-side.com>
2023-06-26 23:53:06 +0200

    generate Base64

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("hello world")
+
+function generateBase64() {
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
+
+      var credentials = username + ":" + password;
+      var encodedCredentials = btoa(credentials);
+
+      console.log("Base64 encoded value: " + encodedCredentials);
+}
